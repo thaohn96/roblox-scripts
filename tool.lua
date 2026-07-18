5fa423b30cd17d790be1418ef054603414e9e607
thaohn96 <69726600+thaohn96@users.noreply.github.com>
2026-07-19 02:26:09 +0700

    Update tool.lua

--- tool.lua
+++ tool.lua
@@ -156,7 +156,7 @@ cornerUp.Parent = upBtnOut
 local downBtnOut = Instance.new("TextButton")
 downBtnOut.Size = UDim2.new(0, 45, 0, 45)
 if isMobile then
-    downBtnOut.Position = UDim2.new(50, 50, 50, 50)
+    downBtnOut.Position = UDim2.new(0, 10, 0, 50)
 else
     downBtnOut.Position = UDim2.new(1, -60, 0.05, 50)
 end
